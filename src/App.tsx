d99f4b6875dd08900e1a0731e62e09aa14f8390c
umutyavuz <umut.yavuz@akinon.com>
2022-06-30 21:36:17 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App: FC = () => {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1
+      }}
+    >
+      <Text>asdfas</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
